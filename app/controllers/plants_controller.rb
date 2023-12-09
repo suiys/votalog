57f88441bb204a6aaaca099a881ee16afbf5b57d
s_abe <s_abe@s-abenoMacBook-Air.local>
2023-12-09 20:20:43 +0900

    [add]新規株登録機能を追加

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -2,9 +2,18 @@ class PlantsController < ApplicationController
   before_action :authenticate_user!
 
   def new
+    @plant = Plant.new
   end
 
   def create
+    @plant = Plant.new(plant_params)
+    @plant.user = current_user
+    if @plant.save
+      redirect_to plant_path(@plant), notice: "#{@plant.name}をマイ多肉棚に追加しました"
+    else
+      flash.now[:alert] = '新しい株の登録に失敗しました'
+      render "new"
+    end
   end
 
   def show
@@ -14,5 +23,9 @@ class PlantsController < ApplicationController
   end
 
   def destroy
+  end
+
+  def plant_params
+    params.require(:plant).permit(:name, :next_water_day, :next_fertilizer_day, :next_replant_day, :image)
   end
 end
