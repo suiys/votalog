3304c3326024618741662e178e64ccca183680f6
s_abe <s_abe@s-abenoMacBook-Air.local>
2023-12-11 08:11:12 +0900

    [update]Plantモデルにバリデーションを追加
    次のお世話予定日を追加する際に、予定入力日時点よりも前の日付を指定するとバリデーションエラーが発生するようにしました

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,6 +1,9 @@
 class Plant < ApplicationRecord
   validates :name, presence: true
   validate :is_file_type_valid?
+  validate :is_next_water_day_after_today?
+  validate :is_next_fertilizer_day_after_today?
+  validate :is_next_replant_day_after_today?
 
   belongs_to :user
 
@@ -12,6 +15,24 @@ class Plant < ApplicationRecord
     valid_file_types = ["image/png", "image/jpg", "image/gif"]
     unless valid_file_types.include?(image.blob.content_type)
       errors.add(:image, "には拡張子が.png, .jpg, .gifのいずれかのファイルを添付してください")
+    end
+  end
+
+  def is_next_water_day_after_today?
+    if next_water_day.present? && next_water_day < Time.zone.today
+      errors.add(:next_water_day, "は今日以降の日付を選択してください")
+    end
+  end
+
+  def is_next_fertilizer_day_after_today?
+    if next_fertilizer_day.present? && next_fertilizer_day < Time.zone.today
+      errors.add(:next_fertilizer_day, "は今日以降の日付を選択してください")
+    end
+  end
+
+  def is_next_replant_day_after_today?
+    if next_replant_day.present? && next_replant_day < Time.zone.today
+      errors.add(:next_replant_day, "は今日以降の日付を選択してください")
     end
   end
 end
