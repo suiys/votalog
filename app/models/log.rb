350cdd5bb3af27a3d2ec5edb6248266256fdf226
s_abe <s_abe@s-abenoMacBook-Air.local>
2023-12-11 23:02:47 +0900

    [fix]light_end_atカラムのバリデーション設定を修正
    comparisonはrails6では未対応のバリデーションヘルパーだったためカスタムバリデーションで設定し直しました

--- app/models/log.rb
+++ app/models/log.rb
@@ -3,8 +3,8 @@ class Log < ApplicationRecord
   validates :memo, length: { maximum: 300 }
   validates :temperature, numericality: true
   validates :humidity, numericality: { in: 0..100 }
-  validates :light_end_at, comparison: { greater_than: :light_start_at, message: "植物育成ライト消灯時刻は点灯時刻よりも後の時刻を入力してください" }
   validate :is_file_type_valid?
+  validate :is_light_end_at_after_light_start_at?
 
   belongs_to :user
   belongs_to :plant
@@ -17,6 +17,12 @@ class Log < ApplicationRecord
     valid_file_types = ["image/png", "image/jpg", "image/gif"]
     unless valid_file_types.include?(image.blob.content_type)
       errors.add(:image, "には拡張子が.png, .jpg, .gifのいずれかのファイルを添付してください")
+    end
+  end
+
+  def is_light_end_at_after_light_start_at?
+    if light_end_at.present? && light_end_at < light_start_at
+      errors.add(:light_end_at, "は点灯時刻よりも後の時刻を入力してください")
     end
   end
 end
