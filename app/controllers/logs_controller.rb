4d3ba74a5f248f3dfa4a18f729455353cc23cf2c
s_abe <s_abe@s-abenoMacBook-Air.local>
2023-12-12 22:55:45 +0900

    [add]ログの新規作成機能を実装

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,8 +1,17 @@
 class LogsController < ApplicationController
   def new
+    @log = Log.new
   end
 
   def create
+    @log = Log.new(log_params)
+    @log.user = current_user
+    if @log.save
+      redirect_to log_path(@log), notice: "新しいログを保存しました"
+    else
+      flash.now[:alert] = "新しいログの保存に失敗しました"
+      render "new"
+    end
   end
 
   def show
@@ -15,5 +24,9 @@ class LogsController < ApplicationController
   end
 
   def destroy
+  end
+
+  def log_params
+    params.require(:log).permit(:start_time, :is_watered, :is_fertilized, :is_replanted, :memo, :image, :temperature, :humidity, :light_start_at, :light_end_at, :plant_id)
   end
 end
