33f395a3a1bfba2fcd7a80a423789609b5491e20
s_abe <s_abe@s-abenoMacBook-Air.local>
2023-12-16 01:58:11 +0900

    [fix]サインアウト時にNoMethodErrorが発生するようになった問題の修正
    トップページのみログイン状況に応じて表示内容を変える想定のため、homeコントローラ上で渡すインスタンス変数もログイン状態のときのみ使用するようにuser_signed_in?メソッドでのチェックを追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,13 @@
 class HomeController < ApplicationController
   def index
-    @user = current_user
-    @todays_water_scheduled_plants = Plant.search_todays_schedules(@user, "water")
-    @todays_fertilizer_scheduled_plants = Plant.search_todays_schedules(@user, "fertilizer")
-    @todays_replant_scheduled_plants = Plant.search_todays_schedules(@user, "replant")
-    @tomorrows_water_scheduled_plants = Plant.search_tomorrows_schedules(@user, "water")
-    @tomorrows_fertilizer_scheduled_plants = Plant.search_tomorrows_schedules(@user, "fertilizer")
-    @tomorrows_replant_scheduled_plants = Plant.search_tomorrows_schedules(@user, "replant")
+    if user_signed_in?
+      @user = current_user
+      @todays_water_scheduled_plants = Plant.search_todays_schedules(@user, "water")
+      @todays_fertilizer_scheduled_plants = Plant.search_todays_schedules(@user, "fertilizer")
+      @todays_replant_scheduled_plants = Plant.search_todays_schedules(@user, "replant")
+      @tomorrows_water_scheduled_plants = Plant.search_tomorrows_schedules(@user, "water")
+      @tomorrows_fertilizer_scheduled_plants = Plant.search_tomorrows_schedules(@user, "fertilizer")
+      @tomorrows_replant_scheduled_plants = Plant.search_tomorrows_schedules(@user, "replant")
+    end
   end
 end
