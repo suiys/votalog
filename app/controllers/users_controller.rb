231d674a27020b0d337a1d77a9165089566e84fe
s_abe <s_abe@s-abenoMacBook-Air.local>
2023-12-19 12:04:56 +0900

    [update]URLから他のユーザーのupdateアクションにアクセスできないようbefore_actionを設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :ensure_correct_user, only: :update
 
   def show
     @user = User.find(current_user.id)
@@ -41,7 +42,16 @@ class UsersController < ApplicationController
     end
   end
 
-  def user_params
-    params.require(:user).permit(:zipcode, :latitude, :longitude)
-  end
+  private
+
+    def user_params
+      params.require(:user).permit(:zipcode, :latitude, :longitude)
+    end
+
+    def ensure_correct_user
+      user = User.find(params[:id])
+      unless user == current_user
+        redirect_to users_settings_path, alert: "自分のユーザー設定以外の変更は行えません"
+      end
+    end
 end
