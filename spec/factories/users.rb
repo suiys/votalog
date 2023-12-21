d0908c138348a60fb8c30e4fe6246cfe43f7e23e
s_abe <s_abe@s-abenoMacBook-Air.local>
2023-12-21 21:58:32 +0900

    [update]userファクトリのemailカラムがユニーク生成されるよう拡張
    Plantモデルのモデルスペックにおいてユニークユーザーを複数人生成したいため、userファクトリをユニークに生成できるよう変更しました

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     name { "testuser1" }
-    email { "test@example.com" }
+    sequence(:email) { |n| "test#{n}@example.com" }
     password { "test12345" }
   end
 end
