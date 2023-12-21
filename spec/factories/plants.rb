9c3cdafe962f6c97a2525b7491e79c8656f1b51c
s_abe <s_abe@s-abenoMacBook-Air.local>
2023-12-21 23:34:56 +0900

    [add]Plantモデルのファクトリを用意し、モデルスペックを実装

---
+++ spec/factories/plants.rb
@@ -0,0 +1,58 @@
+FactoryBot.define do
+  factory :plant do
+    name { "testplant1" }
+    association :user
+  end
+
+  factory :plant_with_png_image, class: Plant do
+    name { "testplant2" }
+    association :user
+
+    after(:build) do |plant|
+      plant.image.attach(
+        io: File.open("spec/fixtures/sample_image.png"),
+        filename: "sample_image.png",
+        content_type: "image/png"
+      )
+    end
+  end
+
+  factory :plant_with_jpg_image, class: Plant do
+    name { "testplant3" }
+    association :user
+
+    after(:build) do |plant|
+      plant.image.attach(
+        io: File.open("spec/fixtures/sample_image.jpg"),
+        filename: "sample_image.jpg",
+        content_type: "image/jpg"
+      )
+    end
+  end
+
+  factory :plant_with_gif_image, class: Plant do
+    name { "testplant4" }
+    association :user
+
+    after(:build) do |plant|
+      plant.image.attach(
+        io: File.open("spec/fixtures/sample_image.gif"),
+        filename: "sample_image.gif",
+        content_type: "image/gif"
+      )
+    end
+  end
+
+  factory :plant_with_text, class: Plant do
+    name { "testplant5" }
+    association :user
+
+    after(:build) do |plant|
+      plant.image.attach(
+        io: File.open("spec/fixtures/sample_image.txt"),
+        filename: "sample_image.txt",
+        content_type: "text/txt"
+      )
+    end
+  end
+end
---
+++ spec/models/plant_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+RSpec.describe Plant, type: :model do
+  describe "バリデーション関連のテスト" do
+    let(:plant) { build(:plant) }
+    let(:plant_without_name) { build(:plant, name: nil) }
+    let(:plant_with_png_image) { build(:plant_with_png_image) }
+    let(:plant_with_jpg_image) { build(:plant_with_jpg_image) }
+    let(:plant_with_gif_image) { build(:plant_with_gif_image) }
+    let(:plant_with_text) { build(:plant_with_text) }
+
+    it "株名称があればバリデーションをパスすること" do
+      expect(plant).to be_valid
+    end
+
+    it "株名称がないとバリデーションをパスしないこと" do
+      plant_without_name.valid?
+      expect(plant_without_name.errors[:name]).to include "を入力してください"
+    end
+
+    it "PNGイメージをPlantモデルにアタッチできること" do
+      expect(plant_with_png_image).to be_valid
+    end
+
+    it "JPEGイメージをPlantモデルにアタッチできること" do
+      expect(plant_with_jpg_image).to be_valid
+    end
+
+    it "GIFイメージをPlantモデルにアタッチできること" do
+      expect(plant_with_gif_image).to be_valid
+    end
+
+    it "textファイルはPlantモデルにアタッチできないこと" do
+      plant_with_text.valid?
+      expect(plant_with_text.errors[:image]).to include "には拡張子が.png, .jpg, .gifのいずれかのファイルを添付してください"
+    end
+  end
+  describe ".search_todays_schedules" do
+    let(:user) { create(:user) }
+    let(:another_user) { create(:user) }
+    let!(:target_plant_to_water_today) { create_list(:plant, 2, next_water_day: Time.zone.today, user: user) }
+    let!(:not_target_user_plant_to_water_today) { create(:plant, next_water_day: Time.zone.today, user: another_user) }
+    let!(:not_target_date_plant_to_water) { create(:plant, next_water_day: Time.zone.tomorrow, user: user) }
+    let!(:plant_to_fertilize_today) { create(:plant, next_fertilizer_day: Time.zone.today, user: user) }
+    let!(:plant_to_replant_today) { create(:plant, next_replant_day: Time.zone.today, user: user) }
+
+    it "今日水やり予定の株が全て抽出できていること" do
+      expect(Plant.search_todays_schedules(user, "water")).to match_array target_plant_to_water_today
+    end
+  end
+  describe ".search_tomorrows_schedules" do
+    let(:user) { create(:user) }
+    let(:another_user) { create(:user) }
+    let!(:target_plant_to_water_tomorrow) { create_list(:plant, 2, next_water_day: Time.zone.tomorrow, user: user) }
+    let!(:not_target_user_plant_to_water_tomorrow) { create(:plant, next_water_day: Time.zone.tomorrow, user: another_user) }
+    let!(:not_target_date_plant_to_water) { create(:plant, next_water_day: Time.zone.today, user: user) }
+    let!(:plant_to_fertilize_today) { create(:plant, next_fertilizer_day: Time.zone.tomorrow, user: user) }
+    let!(:plant_to_replant_today) { create(:plant, next_replant_day: Time.zone.tomorrow, user: user) }
+
+    it "明日水やり予定の株が全て抽出できていること" do
+      expect(Plant.search_tomorrows_schedules(user, "water")).to match_array target_plant_to_water_tomorrow
+    end
+  end
+end
