fe0bc57d310c93d067ee2c216acc30b31b961d81
s_abe <s_abe@s-abenoMacBook-Air.local>
2023-12-28 22:22:21 +0900

    [update]戻り値を変数に格納する必要がないため変数への代入をなくす

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
 
   def format_zipcode(zipcode)
     seven_digits_zipcode = format("%07d", zipcode).to_s
-    formatted_zipcode = seven_digits_zipcode.slice(0..2) + " - " + seven_digits_zipcode.slice(3..6)
+    seven_digits_zipcode.slice(0..2) + " - " + seven_digits_zipcode.slice(3..6)
   end
 
   def get_users_location(zipcode)
