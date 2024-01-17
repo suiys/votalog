c19393ba2601b281766c674d1c4435c5f135e2b4
s_abe <s_abe@s-abenoMacBook-Air.local>
2024-01-18 00:45:11 +0900

    [update]明日のお世話スケジュール表示機能を明日以降の直近のお世話スケジュール表示機能に変更
    明日以降のお世話スケジュールは日付も合わせ、最大5件まで表示する仕様としました

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,15 @@
 class HomeController < ApplicationController
+  MAX_RESULT_COUNT = 5
+
   def index
     if user_signed_in?
       @user = current_user
       @todays_water_scheduled_plants = Plant.search_todays_schedules(@user, "water")
       @todays_fertilizer_scheduled_plants = Plant.search_todays_schedules(@user, "fertilizer")
       @todays_replant_scheduled_plants = Plant.search_todays_schedules(@user, "replant")
-      @tomorrows_water_scheduled_plants = Plant.search_tomorrows_schedules(@user, "water")
-      @tomorrows_fertilizer_scheduled_plants = Plant.search_tomorrows_schedules(@user, "fertilizer")
-      @tomorrows_replant_scheduled_plants = Plant.search_tomorrows_schedules(@user, "replant")
+      @future_water_scheduled_plants = Plant.search_future_schedules(@user, "water").limit(MAX_RESULT_COUNT)
+      @future_fertilizer_scheduled_plants = Plant.search_future_schedules(@user, "fertilizer").limit(MAX_RESULT_COUNT)
+      @future_replant_scheduled_plants = Plant.search_future_schedules(@user, "replant").limit(MAX_RESULT_COUNT)
     end
   end
 end
